c074c5cbd2a2e5d299708216f8476822e9e68396
David Ostos <davidostos2@gmail.com>
2023-12-22 13:40:04 -0400

    creando button component

---
+++ src/components/front/components/materials/buttonPrimary.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: String
+  })
+</script>
+
+<template>
+    <a class="inline-flex px-5 py-3 mx-auto text-base font-medium text-center text-gray-900 border
+     border-green-400 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white
+     dark:border-green-400 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+    >{{ name }}</a>
+</template>
